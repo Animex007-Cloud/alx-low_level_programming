ebe896081e21d8d159c5f3dd8b1818a09db01ff8
Animex007-Cloud <Ejakpoviedeesiri@gmail.com>
2023-07-15 05:13:33 -0700

    hexdigit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Always entry
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	int cha;
+	char hexdigit;
+
+	for (cha = 0; cha <= 16; cha++)
+	if (cha < 10)
+	{
+	hexdigit = 0 + cha;
+	}
+	else hexdigit = 'a' + (cha - 10)
+	{
+	putchar(hexdigit);
+	}
+	putchar('\n');
+	return (0);
+}
