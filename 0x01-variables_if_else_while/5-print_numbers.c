86ea2ed1cada0c596f19cb7c7159fefb3c16af51
Animex007-Cloud <Ejakpoviedeesiri@gmail.com>
2023-07-14 20:29:00 -0700

    base ten

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Always entry
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	char base = '0'
+
+	for (base = '0'; base <= '9'; base++)
+	{
+	putchar(base);
+	}
+	putchar('\n')
+	return (0);
+}
