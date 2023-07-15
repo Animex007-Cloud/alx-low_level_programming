21d84aaf714f74e5f860f39ab1a747f180d714a5
Animex007-Cloud <Ejakpoviedeesiri@gmail.com>
2023-07-15 04:56:26 -0700

    alphabet backwards

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Always entry
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	char ch = 'z';
+
+	for (ch = 'z'; ch > 'a'; ch--)
+	{
+	putchar('z');
+	}
+	putchar('\n');
+	return (0);
+}
