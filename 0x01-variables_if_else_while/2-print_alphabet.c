9ef2e0139ee93c7eddccb255b52fa1c541a1d9fb
Animex007-Cloud <Ejakpoviedeesiri@gmail.com>
2023-07-14 18:12:06 -0700

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Always entry
+ * Return: 0 (Successful)
+ */
+int main()
+{
+
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	putchar(ch);
+	return (0);
+}
