9a75ad1f6f96ce5aea75f419132f5323e55e57d1
Animex007-Cloud <Ejakpoviedeesiri@gmail.com>
2023-07-15 03:43:27 -0700

    base numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Always entry
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	int '0'
+
+	for ('0' <= 9; '0'++)
+	{
+	putchar('0');
+	}
+	puuchar('\n');
+	return (0);
+}
