ab58afe184cddc52be21bf50904ede0b0ff0ebe9
Animex007-Cloud <Ejakpoviedeesiri@gmail.com>
2023-07-18 16:58:16 -0700

    function loop

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,27 +11,30 @@ int main(void)
 	int num2;
 	int num3;
 
-	for (num0 = 0; num0 <= 8; num0++)
-	for (num1 = 0; num1 <= 9; num1++)
+	for (num0 = 0; num0 <= 9; num0++)
 	{
-		putchar(num0 + '0');
-		putchar(num1 + '0');
-
-		if (num0 != 8 || num1 != 9)
+		for (num1 = 0; num1 <= 8; num1++)
 		{
-		putchar(' ');
-		}
-			for (num2 = 0; num2 <= 9; num2++)
-			for (num3 = (num2 + 1); num3 <= 9; num3++)
+			putchar(num0 + '0');
+			putchar(num1 + '0');
+			if (num0 != 9 || num1 != 8)
 			{
-				putchar(num2 + '0');
-				putchar(num3 + '0');
-				
-				if (num2 != 9 || num3 != 9)
+				putchar(' ');
+
 				{
-					putchar(',');
-					putchar(' ');
+					for (num2 = 0; num2 <= 9; num2++)
+					for (num3 = (num2 + 1); num3 <= 9; num3++)
+						{
+							putchar(num2 + '0');
+							putchar(num3 + '0');
+							if (num2 != 9 || num3 != 9)
+							{
+								putchar(',');
+								putchar(' ');
+							}
+						}
 				}
+			}
 		}
 	}
 	putchar('\n');
