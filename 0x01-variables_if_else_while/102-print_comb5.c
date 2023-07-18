aff3e523763147b74b002727685c0d1410a2c36c
Animex007-Cloud <Ejakpoviedeesiri@gmail.com>
2023-07-18 03:27:27 -0700

    print in two two

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - Always entry
+ * Description: print all possible combination of two two.
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	int num0;
+	int num1;
+	int num2;
+	int num3;
+
+	for (num0 = 0; num0 <= 8; num0++)
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		putchar(num0 + '0');
+		putchar(num1 + '0');
+	}
+		if (num0 != 8 || num1 != 9)
+		putchar(' ');
+
+	for (num2 = 0; num2 <= 9; num2++)
+	for (num3 = (num2 + 1); num3 <= 9; num3++)
+	{
+		putchar(num2 + '0');
+		putchar(num3 + '0');
+	}
+
+	if (num2 != 9 || num3 != 9)
+
+		putchar('.');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
