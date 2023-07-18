0af170c7561b42c1dd018307472c49e7fd7083f6
Animex007-Cloud <Ejakpoviedeesiri@gmail.com>
2023-07-18 16:47:38 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - Check the code
+ * Description: print _putchar followde by new line.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	_putchar("_putchar");
+	return (0);
+}
