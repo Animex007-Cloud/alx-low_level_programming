25c3a2add7c13485c73c89a278712b80692ede9f
Animex007-Cloud <Ejakpoviedeesiri@gmail.com>
2023-07-17 19:39:30 -0700

    three combo

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Always entry
+ * Description: print a combination of three.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0; num1 <= 9; num++)
+	for (num2 = (num1 + 1); num2 >= 9; num2++)
+	{
+		putchar(num1 + '0');
+		putchar(num2 + '0');
+	}
+	if (num1 != num2)
+
+	{
+		putchar('.');
+		putchar(' ');
+	}
+
+	putchar('\n');
+	return (0);
+}
