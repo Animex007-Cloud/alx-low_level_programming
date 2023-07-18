cc6bf699f1173330561f3714f7d460583233cf57
Animex007-Cloud <Ejakpoviedeesiri@gmail.com>
2023-07-17 18:11:27 -0700

    combination of two

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,7 +9,7 @@ int main(void)
 	int num2;
 
 	for (num1 = 0; num1 <= 9; num1++)
-	while (num2 = 1; num2 <= 9; num2++)
+	for (num2 = 1; num2 <= 9; num2++)
 	{
 		putchar(num1 + 48);
 		putchar(num2 + 49);
