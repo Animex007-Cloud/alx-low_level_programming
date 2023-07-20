7f0b685f0f25bc96fbd73a4a56fd098f0f77771a
Animex007-Cloud <Ejakpoviedeesiri@gmail.com>
2023-07-20 09:32:09 -0700

    add return

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * Description: Return added result
+ * @e: int declared
+ * @f: int declareed
+ * Return: 0
+ */
+
+int add(int e, int f)
+{
+	return (e + f);
+}
