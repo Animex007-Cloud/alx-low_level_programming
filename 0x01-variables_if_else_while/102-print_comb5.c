df200b211f6b6c3f8cedf629bf29d97816d5f1b2
Animex007-Cloud <Ejakpoviedeesiri@gmail.com>
2023-07-20 10:28:56 -0700

    final commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,31 +11,28 @@ int main(void)
 	int num2;
 	int num3;
 
-	for (num0 = 0; num0 <= 9; num0++)
 	{
-		for (num1 = 0; num1 <= 8; num1++)
-		{
-			putchar(num0 + '0');
-			putchar(num1 + '0');
-			if (num0 != 9 || num1 != 8)
-			{
-				putchar(' ');
-
-				{
-					for (num2 = 0; num2 <= 9; num2++)
-					for (num3 = (num2 + 1); num3 <= 9; num3++)
-						{
-							putchar(num2 + '0');
-							putchar(num3 + '0');
-							if (num2 != 9 || num3 != 9)
-							{
-								putchar(',');
-								putchar(' ');
-							}
-						}
-				}
-			}
-		}
+	for (num0 = 0; num0 < 10; num0++)
+	for (num1 = 0; num1 < 9; num1++)
+	{
+		putchar(num0 + '0');
+		putchar(num1 + '0');
+	}
+	if (num0 != 9 || num1 != 8)
+	{
+		putchar(' ');
+	}
+	for (num2 = 0; num2 <= 9; num2++)
+	for (num3 = (num2 + 1); num3 <= 9; num3++)
+	{
+		putchar(num2 + '0');
+		putchar(num3 + '0');
+	}
+	if (num2 != 9 || num3 != 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
 	}
 	putchar('\n');
 	return (0);
