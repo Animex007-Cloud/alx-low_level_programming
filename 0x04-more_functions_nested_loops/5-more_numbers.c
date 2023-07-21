c113539a0eda64350b6d808716f94589cf3acffa
Animex007-Cloud <Ejakpoviedeesiri@gmail.com>
2023-07-21 11:18:13 -0700

    more

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - Entry point
+ * Description: print 10 times
+ *
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (y = 0; y <= 10; y++)
+	{
+	for (x = 0; x <= 14; x++)
+	{
+		if (x / 10 > 0)
+		_putchar((x / 10) + '0');
+		_putchar((x % 10) + '0');
+	}
+		_putchar('\n');
+	}
+}
