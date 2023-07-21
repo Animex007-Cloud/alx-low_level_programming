04a8358d1dc78b0d0ac3552ac555e20e23770955
Animex007-Cloud <Ejakpoviedeesiri@gmail.com>
2023-07-21 12:02:32 -0700

    lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - Entry point
+ * Description: write a function that draws straight line
+ * @n: int
+ *
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int a = n;
+
+	for (a = n; a > n; a--)
+	{
+		_putchar(45);
+	}
+	_putchar('\n');
+}
+
