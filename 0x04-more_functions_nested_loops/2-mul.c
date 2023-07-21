fdb6467f2b20c58e1a68625396b18b06821592bd
Animex007-Cloud <Ejakpoviedeesiri@gmail.com>
2023-07-21 09:18:36 -0700

    mul two int

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * Description: multiply two integer
+ *
+ * @a: input
+ * @b: second input
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
