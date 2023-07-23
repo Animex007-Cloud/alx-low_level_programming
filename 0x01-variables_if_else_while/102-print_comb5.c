39d7f58e88390039809d3cf8f7d40ac120624d75
Animex007-Cloud <Ejakpoviedeesiri@gmail.com>
2023-07-22 18:33:11 -0700

    num 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,31 +8,20 @@ int main(void)
 {
 	int num0;
 	int num1;
-	int num2;
-	int num3;
 
+	for (num0 = 0; num0 < 100; num0++)
 	{
-	for (num0 = 0; num0 < 10; num0++)
-	for (num1 = 0; num1 < 9; num1++)
-	{
-		putchar(num0 + '0');
-		putchar(num1 + '0');
-	}
-	if (num0 != 9 || num1 != 8)
-	{
-		putchar(' ');
-	}
-	for (num2 = 0; num2 <= 9; num2++)
-	for (num3 = (num2 + 1); num3 <= 9; num3++)
-	{
-		putchar(num2 + '0');
-		putchar(num3 + '0');
-	}
-	if (num2 != 9 || num3 != 9)
-	{
-		putchar(',');
-		putchar(' ');
-	}
+		for (num1 = num0 + 1; num1 < 100; num1++)
+		{
+			putchar((num0 / 10) + '0');
+			putchar((num0 % 10) + '0');
+			putchar(' ');
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			if (!(num0 == 98 && num1 == 99))
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
