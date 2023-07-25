7bc52c3adeaaf6b535d8f093b6ae92bc692e80b0
Animex007-Cloud <Ejakpoviedeesiri@gmail.com>
2023-07-25 07:21:48 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ * Description: function that takes pointer to an int
+ * @n: pointer
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
