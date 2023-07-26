a7e3f8a9f8aaa2ea92f8cc6babf71ee2bca8d5bb
Animex007-Cloud <Ejakpoviedeesiri@gmail.com>
2023-07-26 04:02:58 -0700

    print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev - Entry point
+ * Description: print in reverse
+ *
+ * @s: declared character
+ *
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+	int x;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	for (x = len; x >= 0; x--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
