c8fbd85f9d19faece7745c8d1e48f7f463250d4f
Animex007-Cloud <Ejakpoviedeesiri@gmail.com>
2023-08-07 06:39:57 -0700

    print arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Descriptoin: A function that prints the number of arguments passed to it
+ * @argc: number holder
+ * @argv: string holder
+ * Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
