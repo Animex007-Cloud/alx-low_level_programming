c3e930ba39b3bb8a6343bea676fea09ffeaf529a
Animex007-Cloud <Ejakpoviedeesiri@gmail.com>
2023-08-10 10:26:54 -0700

    con

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,28 +14,33 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1 = 0, len2 = 0, total = 0;
-	char *concatenated;
+	int y;
+	unsigned int z;
+	char *con;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-
-	if (n  >= len2)
-		n = len2;
-
-	total = len1 + n + 1;
-	concatenated = malloc(total);
-	if (concatenated == NULL)
+	y = 0;
+	while (s1[y] != '\0')
+		y++;
+	con = malloc(sizeof(char) * (y + n + 1));
+	if (con == NULL)
 		return (NULL);
+	y = z = 0;
 
-	strncpy(concatenated, s1, len1);
-	strncat(concatenated, s2, n);
-	concatenated[total - 1] = '\0';
+	while (s1[y] != '\0')
+	{
+		con[y] = s1[y];
+		y++;
+	}
 
-	return (concatenated);
+	while (z < n && s2[z] != '\0')
+	{
+		con[y] = s2[z];
+		y++, z++;
+	}
+	con[y] = '\0';
+	return (con);
 }
