7a6a94d1c2354ac49507e3c20e7f08524e0837db
Animex007-Cloud <Ejakpoviedeesiri@gmail.com>
2023-08-14 12:23:36 -0700

    compile

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: A program that prints the name of the file
+ * it was compiled from
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n" __BASE_FILE__);
+	return (0);
+}
