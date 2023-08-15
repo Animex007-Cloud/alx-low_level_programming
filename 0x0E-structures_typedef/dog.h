2a959acf7a2d88887d9854808d51bec26f559d2e
Animex007-Cloud <Ejakpoviedeesiri@gmail.com>
2023-08-14 19:13:23 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Define a new struct for dog header
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
 struct dog
 {
 	char *name;
 	float age;
-	char owner;
+	char *owner;
 };
+
 #endif
