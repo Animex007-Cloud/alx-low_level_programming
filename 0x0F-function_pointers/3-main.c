68dcedfe680513056138b561f2dd5a721f0512ea
Animex007-Cloud <Ejakpoviedeesiri@gmail.com>
2023-08-16 17:52:56 -0700

    add

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * Description: A function that process parameters
+ * @argc: int argument
+ * @argv: pointer to argument
+ * Return: 0
+ */
+int main(int argc, shar *argv[])
+{
+	int num1, num2, calc;
+	int (*res)(int , int);
+	char *get_op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	get_op = argv[2];
+
+	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	res = get_op_function(get_op);
+	calc = res(num1, num2);
+
+	printf("%d\n", calc);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+#include "3-cal.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - entry point
+ * @a: first input
+ * @b: second input
+ * Return: difference
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - entry point
+ * @a: first input
+ * @b: second input
+ * Return: differrence
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - entry point
+ * @a: first input
+ * @b: second input
+ * Return: difference
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - entry point
+ * @a: first input
+ * @b: second input
+ * Return: difference
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - entry point
+ * @a: first input
+ * @b: second input
+ * Return: different
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
