5b3802f0f28e9d8aa46eb4812bd13545fd2bb100
Animex007-Cloud <ejakpoviedeesiri@gmail.com>
2023-08-24 12:50:12 -0700

    node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_node - entry point
+ * Description: code that adds a new node at the beginning of a list
+ *
+ * @head: pointer to pointer
+ * @str: pointer to char
+ * Return: address of new address
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *z;
+	unsigned int y;
+
+	while (str[y])
+		y++;
+
+	z = malloc(sizeof(list_t));
+	if (!z)
+	{
+		return (NULL);
+	}
+
+	z->str = strdup(str);
+	z->y = y;
+	z->next = (*head);
+	(*head) = z;
+
+	return (*head);
+
+}
