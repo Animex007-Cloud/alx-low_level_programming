cff14470196de1c2ba456dda55de0a4384aae6ae
Animex007-Cloud <ejakpoviedeesiri@gmail.com>
2023-08-24 12:07:06 -0700

    lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,21 +12,20 @@ size_t print_list(const list_t *h)
 {
 	size_t z = 0;
 
-	if (h == NULL)
-		return (0);
-
-	do
 	{
-		if (!h->str)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", h->len, h->str);
+		if (h == NULL)
+			return (0);
 
-		h = h->next;
-		z++;
+		while (h)
+		{
+			if (!h->str)
+				printf("[0] (nil)\n");
+
+			else
+				printf("[%u] %s\n", h->len, h->str);
+			h = h->next;
+			z++;
+		}
+		return (z);
 	}
-	while (h != NULL);
-
-
-	return (z);
 }
