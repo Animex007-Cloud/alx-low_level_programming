a59f100f91a21a29a17a81087f6864966538cd25
Animex007-Cloud <ejakpoviedeesiri@gmail.com>
2023-08-24 11:38:19 -0700

    list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * print_list - entry point
+ * Description: A function that prints all the elements of a list
+ *
+ * @h: pointer to list
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t z = 0;
+
+	if (h == NULL)
+		return (0);
+
+	do
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+
+		h = h->next;
+		z++;
+	}
+	while (h != NULL);
+
+
+	return (z);
+}
