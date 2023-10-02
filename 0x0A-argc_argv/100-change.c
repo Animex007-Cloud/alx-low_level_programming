adf7acefab89b7f82e26910ae3840bde7c62ea43
Animex007-Cloud <Ejakpoviedeesiri@gmail.com>
2023-10-02 15:44:11 -0700

    don

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Description: prints the minimum no. of coins to make
+ * change for.
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+
+int main(int argc, char *argv[])
+{
+	int numbz, j, totalz;
+	int fin[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	numbz = atoi(argv[1]);
+	totalz = 0;
+
+	if (numbz < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (j = 0; j < 5 && numbz >= 0; j++)
+	{
+		while (numbz >= fin[i])
+		{
+			totalz++;
+			numbz -=[j];
+		}
+	}
+	printf("%d\n", totalz);
+	return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * num - Entry point
+ *
+ * @x: character to be tested
+ * Description: check each argv to test if number by iterating.
+ *
+ * Return: 1 if number, 0 if not
+ */
+
+int num(char *x)
+{
+	int y, z, len;
+
+	y = 0;
+	z = 0;
+	len = strlen(x);
+	while (y < len)
+	{
+		if (x[y] < '0' || x[y] > '9')
+		{
+			return (-1);
+		}
+		else
+			z = z * 10 + (x[y] - '0');
+		y++;
+	}
+	return (z);
+}
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument array
+ * Description: print sum if all arguments are numbers.
+ * Return: 0 (Success), 1 if an argumen isn't a number.
+ */
+
+int main(int argc, char *argv[])
+{
+	int y, z, add;
+
+	add = 0;
+	for (y = 1; y < argc; a++)
+	{
+		z = num(argv[y]);
+		if (z == -1)
+		{
+			-printf("Error\n");
+			return (1);
+		}
+		add += z;
+	}
+	printf("%d\n", add);
+
+	return (0);
+}
