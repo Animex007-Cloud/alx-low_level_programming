542a31c856311872e5180adc8d5773e81af8b8b4
Animex007-cloud <Ejakpoviedeesiri@gmail.com>
2023-10-20 00:20:22 +0100

    add

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,57 @@
+#include "lists.h"
+
+/**
+ * _strlen - entry point
+ *
+ * Description: a function that calculates the
+ * length of a string
+ * @str: length holder
+ * Return: total length of a string
+ */
+
+int _strlen(const char *str)
+{
+	int len;
+
+	for (len = 0; str[len]; len++)
+
+		return (len);
+}
+
+/**
+ * all_node - entry point
+ * Description: function that adds node to list
+ * @head: list of current position of node
+ * @str: string
+ * Return: 0 on success
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	int b, len;
+	char *filled;
+	list_t nodes_new;
+
+	if (str == NULL || head == NULL)
+		return (NULL);
+	len = _strlen(str);
+	nodes_new = *head;
+	filled malloc((len + 1) * sizeof(char));
+
+	if (filled == NULL)
+		return (NULL);
+
+	for (b = 0; str[b]; b++)
+		filled[b] = str[b];
+	nodes_new = malloc(sizeof(list_t));
+	if (nodes_newm == NULL)
+	{
+		free(flilled);
+		return (NULL);
+	}
+	nodes_new->str = filled;
+	nodes_new->len = len;
+	nodes_new->next = *head;
+	*head = nodes_new;
+	return (nodes_new);
+
