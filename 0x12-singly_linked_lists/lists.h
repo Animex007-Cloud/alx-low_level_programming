1963cce2d74f6f076d92a08ff5dbb2b23c6e0123
Animex007-cloud <Ejakpoviedeesiri@gmail.com>
2023-10-19 21:25:57 +0100

    node

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,26 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlid.h>
+#include <string.h>
+
+/**
+ * struct list_s - entry point
+ *
+ * Description: prototypes and singly linked list
+ * @str: pointer to strings
+ * @len: the length of strings
+ * @next: a pointer to nodes.
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t
+
+size_t print_list(const list_t *h);
+
+#endif
