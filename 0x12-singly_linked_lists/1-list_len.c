96ea821b1ca7bc8a23e5faa0b5de624ff45aef04
Animex007-Cloud <Ejakpoviedeesiri@gmail.com>
2023-10-19 15:15:52 -0700

    len

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,4 +3,21 @@
 /**
  * list_len - entry point
  *
- * Description:
+ * Description: A finction that prints the
+ * length of list link
+ * @h: the list linked
+ *
+ * Return: num of element.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t sum = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		sum++;
+	}
+	return (sum);
+}
