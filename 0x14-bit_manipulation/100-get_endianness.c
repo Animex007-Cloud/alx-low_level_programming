cec794ddcf5654d59b4e187a3a274b45ce2f4167
Animex007-cloud <Ejakpoviedeesiri@gmail.com>
2023-10-27 00:53:42 +0100

    bits

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianness - entry point
+ * Description: A function that checks for endianness
+ * Return: 1 if little endian and 0 if big endian
+ */
+
+int get_endianness(void)
+{
+	int b = 1;
+	char *c;
+
+	c = (char *)&b;
+	return (*c);
+}
